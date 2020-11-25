f75156b61998e56847b27f6b99009386920e44db
Tugdual Sarazin <tugdual.sarazin@gmail.com>
2020-11-25 12:01:16 +0100

    Update OutputPath.py

--- 2D_to_2D_neural_style_transfer/mask-guided-neural-style/OutputPath.py
+++ 2D_to_2D_neural_style_transfer/mask-guided-neural-style/OutputPath.py
@@ -4,10 +4,11 @@ import os
 class OutputPath:
     @staticmethod
     def path(args):
-        content_img = os.path.split(os.path.splitext(args.content_img)[0])[1]
-        content_weight = int(args.content_weight)
-        style_img = os.path.split(os.path.splitext(args.style_img)[0])[1]
-        style_weight = int(args.style_weight)
-        iteration = args.iteration
-        file_name = f'{content_img}-{content_weight}_TO_{style_img}-{style_weight}_iter{iteration}.png'
+        file_name = '{content_img}-{content_weight}_TO_{style_img}-{style_weight}_iter{iteration}.png'.format(
+            content_img=os.path.split(os.path.splitext(args.content_img)[0])[1],
+            content_weight=int(args.content_weight),
+            style_img=os.path.split(os.path.splitext(args.style_img)[0])[1],
+            style_weight=int(args.style_weight),
+            iteration=args.iteration,
+        )
         return os.path.join(args.output_dir, file_name)
